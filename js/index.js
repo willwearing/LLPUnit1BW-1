dd2631d6cb056ddcff5fbfca8de3d7df8ecfc364
willwearing <willwearing@gmail.com>
2020-08-24 14:22:47 -0600

    added tab buttons and styled them. Tabs are currently not loading. Need to bug test and figure out why the JS isn't working

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-// JS goes here
+// services js section start
+function openPage(pageName, elmnt, color) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+  
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+
+  
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
+} 
+// secrives js section end
