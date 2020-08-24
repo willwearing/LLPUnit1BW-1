e06bfcd164b05e4496e30304adb78aa466bc1fab
willwearing <willwearing@gmail.com>
2020-08-24 15:03:59 -0600

    fixed js bug and now all tabs are properly showing. CSS styling now needs work

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
 // services js section start
 function openPage(pageName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-  
-    // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
 
-  
-    // Get the element with id="defaultOpen" and click on it
-    document.getElementById("defaultOpen").click();
-} 
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
 // secrives js section end
