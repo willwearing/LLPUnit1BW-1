39b8f8c4d2626d63cfcd254168222c6b304297ad
Nathaniel Patterson <odst0016@gmail.com>
2020-08-24 21:30:41 -0400

    decided how to do menu

--- js/index.js
+++ js/index.js
@@ -1,36 +1,10 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
-// gets input from open buttons
-openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = document.querySelector(button.dataset.modalTarget)
-        openModal(modal)
-    })
-})
-// gets input from clicking outside of modal
-// overlay.addEventListener('click', () => {
-//     const Modals = document.querySelectorAll('.modal.active')
-//     Modals.forEach(modal => {
-//         closeModal(modal)
-//     })
-// })
-// gets input from close button
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = button.closest('.modal')
-        closeModal(modal)
-    })
-})
-// opens the modal
-function openModal(modal) {
-    if (modal == null) return
-    modal.classList.add('active')
-    overlay.classList.add('active')
-}
-// closes the modal
-function closeModal(modal) {
-    if (modal == null) return
-    modal.classList.remove('active')
-    overlay.classList.remove('active')
+// Menu
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " actmenu";
+  } else {
+    x.className = "topnav";
+  }
 }
